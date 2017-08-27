7c442f4bf1401f463f188cc73068d7abf6d37c54
Ian Zhang <ianzhang1986@gmail.com>
2017-08-27 10:13:21 -0700

    [Feature] TODO: get_tensor_by_names

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
 
-# def load_vgg(sess, vgg_path):
-#     """
-#     Load Pretrained VGG Model into TensorFlow.
-#     :param sess: TensorFlow Session
-#     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
-#     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
-#     """
-#     # TODO: Implement function
-#     #   Use tf.saved_model.loader.load to load the model and weights
-#     tf.saved.model.loader.load(sess);
-#     vgg_tag = 'vgg16'
-#     vgg_input_tensor_name = 'image_input:0'
-#     vgg_keep_prob_tensor_name = 'keep_prob:0'
-#     vgg_layer3_out_tensor_name = 'layer3_out:0'
-#     vgg_layer4_out_tensor_name = 'layer4_out:0'
-#     vgg_layer7_out_tensor_name = 'layer7_out:0'
-#
-#     return None, None, None, None, None
-# tests.test_load_vgg(load_vgg, tf)
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    # TODO: Implement function
+    #   Use tf.saved_model.loader.load to load the model and weights
+    vgg_tag = 'vgg16'
+    vgg_input_tensor_name = 'image_input:0'
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved.model.loader.load(sess, [vgg_tag], vgg_path);
+    return None, None, None, None, None
+tests.test_load_vgg(load_vgg, tf)
 #
 #
 # def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
@@ -105,11 +105,12 @@ def run():
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
-    #
-    #     # OPTIONAL: Augment Images for better results
-    #     #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-    #
-    #     # TODO: Build NN using load_vgg, layers, and optimize function
+
+        # OPTIONAL: Augment Images for better results
+        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+
+        # TODO: Build NN using load_vgg, layers, and optimize function
+        load_vgg(sess, vgg_path);
     #
     #     # TODO: Train NN using the train_nn function
     #
